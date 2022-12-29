bacec5ea29d5c38283dcafeb269f73a4b1fb3b72
Pedro Cavalcante <pedrodanilo48@gmail.com>
2022-12-28 23:53:15 -0300

    titulo

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 let corAtual = 'black';
+let screen = document.querySelector('#tela');
 document.querySelectorAll('.colorArea .color').forEach(item =>{
     item.addEventListener('click' , colorClickEvent);
 });
 function colorClickEvent(e) {
     let color = e.target.getAttribute('data-color');
     console.log("COR CLICADA: ", color);
+    corAtual = color;
+    document.querySelector('.color.active').classList.remove('active');
+    e.target.classList.add('active');
 }
