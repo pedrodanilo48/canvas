e2538cf76a81c90d7c09bd1abc9f405d9072c6e2
Pedro Cavalcante <pedrodanilo48@gmail.com>
2023-01-08 13:55:15 -0300

    atualizacao js

--- script.js
+++ script.js
@@ -1,37 +1,41 @@
-let corAtual = 'black';
-let screen = document.querySelector('#tela');
-let contexto = screen.getContext('2d');
-let podeDesenhar = false;
+let currentColor = 'black';
+let canDraw = false;
 let mouseX = 0;
 let mouseY = 0;
+
+let screen = document.querySelector('#tela');
+let ctx = screen.getContext('2d');
 
 document.querySelectorAll('.colorArea .color').forEach(item =>{
     item.addEventListener('click' , colorClickEvent);
 });
 
-screen.addEventListener('mousedown', mouseDownEvent)
-screen.addEventListener('mousemove', mouseMoveEvent)
-screen.addEventListener('mousemove', mouseUpEvent)
+screen.addEventListener('mousedown', mouseDownEvent);
+screen.addEventListener('mousemove', mouseMoveEvent);
+screen.addEventListener('mousemove', mouseUpEvent);
+document.querySelector('.clear').addEventListener('click', clearScreen);
 
 function colorClickEvent(e) {
     let color = e.target.getAttribute('data-color');
-    console.log("COR CLICADA: ", color);
-    corAtual = color;
+    currentColor = color;
+
     document.querySelector('.color.active').classList.remove('active');
     e.target.classList.add('active');
 }
-function mouseDownEvent(){
-    podeDesenhar = true;
+function mouseDownEvent(e){
+    canDraw = true;
+    mouseX = e.pageX - screen.offsetLeft;
+    mouseY = e.pageY - screen.offsetTop;
 }
-function mouseMoveEvent(){
-     if (podeDesenhar){
-        desenhar(e.pageX, e.pageY);
+function mouseMoveEvent(e){
+     if (canDraw){
+        draw(e.pageX, e.pageY);
      }
 }
 function mouseUpEvent(){
-    podeDesenhar = false;
+    canDraw = false;
 }
-function desenhar(x , y){
+function draw(x , y){
     let pointX = x - screen.offsetLeft;
     let pointY = y - screen.offsetTop;
 
@@ -46,4 +50,8 @@ function desenhar(x , y){
 
     mouseX = pointX;
     mouseY = pointY;
+}
+function clearScreen(){
+    ctx.setTransform(1,0,0,1,0,0);
+    ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height);
 }
